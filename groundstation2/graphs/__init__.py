42c17e7d6afca1b5c95a6ad89169b5337cf75a40
AnshThayil <Ansh.Thayil@yahoo.in>
2018-11-12 21:34:49 -0800

    Using generator.

--- groundstation2/graphs/__init__.py
+++ groundstation2/graphs/__init__.py
@@ -12,11 +12,11 @@ class GraphDisplayWidget(QtGui.QDialog):
 
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
-        
+
         self.view = pg.GraphicsView()
         self.graphpaper = pg.PlotItem()
         self.view.setCentralWidget(self.graphpaper)
-        self.PenStyle = {'width': 1.5, 'color': (0,0,0)}
+        self.PenStyle = {'width': 1.5, 'color':(0,0,0)}
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.view)
         self.setLayout(layout)
@@ -31,6 +31,17 @@ class GraphDisplayWidget(QtGui.QDialog):
         self.graphpaper.plot(self.TimeData, self.AltitudeData, pen = self.PenStyle)
 
 if __name__ == "__main__":
+
     app = QtGui.QApplication(sys.argv)
     widg = GraphDisplayWidget()
+    timer = pg.QtCore.QTimer()
+    def sendData():
+        yield lambda: widg.newData.emit({"altitude": 0, "timestamp": 0})
+        yield lambda: widg.newData.emit({"altitude": 1, "timestamp": 1})
+        yield lambda: widg.newData.emit({"altitude": 2, "timestamp": 2})
+        yield lambda: widg.newData.emit({"altitude": 3, "timestamp": 3})
+        yield lambda: widg.newData.emit({"altitude": 4, "timestamp": 4})
+    a = sendData()
+    timer.timeout.connect(next(a))
+    timer.start(2000)
     app.exec_()
