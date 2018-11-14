5ebe655bbf8f20f8729757a9b71c6c7da772a9c9
AnshThayil <Ansh.Thayil@yahoo.in>
2018-11-13 16:17:36 -0800

    Added velocity graph.

--- groundstation2/graphs/__init__.py
+++ groundstation2/graphs/__init__.py
@@ -12,23 +12,39 @@ class GraphDisplayWidget(QtGui.QDialog):
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
 
-        self.view = pg.GraphicsView()
-        self.graphpaper = pg.PlotItem()
-        self.view.setCentralWidget(self.graphpaper)
+        self.view1 = pg.GraphicsView()
+        self.view2 = pg.GraphicsView()
+        self.graphpaper1 = pg.PlotItem()
+        self.graphpaper2 = pg.PlotItem()
+        self.view1.setCentralWidget(self.graphpaper1)
+        self.view2.setCentralWidget(self.graphpaper2)
         self.PenStyle = {'width': 1.5, 'color':(0,0,0)}
         layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.view)
+        layout.addWidget(self.view1)
+        layout.addWidget(self.view2)
         self.setLayout(layout)
         self.show()
         self.TimeData = []
         self.AltitudeData = []
+        self.VelocityData = []
+        self.alt = 0
+        self.t = 0
         self.newData.connect(self.update)
 
     def update(self, new_data):
         self.TimeData.append(new_data["timestamp"])
         self.AltitudeData.append(new_data["altitude"])
-        self.graphpaper.plot(self.TimeData, self.AltitudeData, pen = self.PenStyle)
-
+        self.graphpaper1.plot(self.TimeData, self.AltitudeData, pen = self.PenStyle)
+        if new_data["timestamp"] != 0:
+            alt_difference = new_data["altitude"] - self.alt
+            t_difference = new_data["timestamp"] - self.t
+            vel = alt_difference/t_difference
+            self.VelocityData.append(vel)
+            self.graphpaper2.plot(self.TimeData, self.VelocityData, pen = self.PenStyle)
+        else:
+            self.VelocityData.append(0)
+        self.alt =  new_data["altitude"]
+        self.t = new_data["timestamp"]
 if __name__ == "__main__":
 
     app = QtGui.QApplication(sys.argv)
